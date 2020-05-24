e63be27110f12a17dbfd04e900c9f37f5e9e285f
Martin <56932913+mmr011@users.noreply.github.com>
2020-05-23 23:03:03 -0400

    setup of backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const errorHandler = require('errorhandler');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./router/router');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorHandler());
+app.use(morgan('tiny'));
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
